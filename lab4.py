4c37b54966b0cb370f924efcb8b1fe05ee4a4bc0
Alex <90724908+lofty1337@users.noreply.github.com>
2023-12-18 16:43:18 +0200

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -1,57 +1,82 @@
 import time
 
+# Интерфейс команды
+class Command:
+    def execute(self):
+        pass
 
+    def undo(self):
+        pass
+
+# Конкретная команда для действия с клавишей
+class KeyActionCommand(Command):
+    def __init__(self, keyboard, key, action):
+        self.keyboard = keyboard
+        self.key = key
+        self.action = action
+
+    def execute(self):
+        self.keyboard.press_key(self.key, self.action)
+
+    def undo(self):
+        print(f"Undoing key action: {self.key}")
+
+# Конкретная команда для действия с комбинацией клавиш
+class KeyCombinationCommand(Command):
+    def __init__(self, keyboard, combination, action):
+        self.keyboard = keyboard
+        self.combination = combination
+        self.action = action
+
+    def execute(self):
+        self.keyboard.press_key_combination(self.combination, self.action)
+
+    def undo(self):
+        print(f"Undoing key combination action: {self.combination}")
+
+# Инвокер (в данном случае, сам класс VirtualKeyboard)
 class VirtualKeyboard:
     def __init__(self):
-        self.key_actions = {}
-        self.key_combinations = {}
-        self.history = []
+        self.command_history = []
 
-    def assign_key_action(self, key, action):
-        self.key_actions[key] = action
+    def assign_command(self, command):
+        self.command_history.append(command)
 
-    def assign_key_combination(self, combination, action):
-        combination = tuple(combination)
-        self.key_combinations[combination] = action
+    def execute_commands(self):
+        for command in self.command_history:
+            command.execute()
 
-    def press_key(self, key):
-        if key in self.key_actions:
-            action = self.key_actions[key]
-            self.history.append(action)
-            print(f"Pressed key '{key}'. Action: {action}")
-        else:
-            print(f"No action assigned to key '{key}'.")
+    def undo_last_command(self):
+        if self.command_history:
+            command = self.command_history.pop()
+            command.undo()
 
-    def press_key_combination(self, combination):
-        combination = tuple(combination)
-        if combination in self.key_combinations:
-            action = self.key_combinations[combination]
-            self.history.append(action)
-            print(f"Pressed key combination {combination}. Action: {action}")
-        else:
-            print(f"No action assigned to key combination {combination}.")
+    def press_key(self, key, action):
+        print(f"Pressed key '{key}'. Action: {action}")
 
-    def undo_last_action(self):
-        if self.history:
-            action = self.history.pop()
-            print(f"Undoing last action: {action}")
-        else:
-            print("No actions to undo.")
+    def press_key_combination(self, combination, action):
+        print(f"Pressed key combination {combination}. Action: {action}")
 
 
 # Пример использования
 keyboard = VirtualKeyboard()
-keyboard.assign_key_action('B', 'Action B')
-keyboard.assign_key_combination(['D', 'E'], 'Action DE')
 
-keyboard.press_key('B')
-time.sleep(1)
-keyboard.press_key_combination(['D', 'E'])
+# Создаем команды
+key_action_command = KeyActionCommand(keyboard, 'B', 'Action B')
+key_combination_command = KeyCombinationCommand(keyboard, ['D', 'E'], 'Action DE')
+
+# Назначаем команды клавиатуре
+keyboard.assign_command(key_action_command)
+keyboard.assign_command(key_combination_command)
+
+# Выполняем команды
+keyboard.execute_commands()
 time.sleep(1)
 
-keyboard.undo_last_action()
+# Отменяем последнюю команду
+keyboard.undo_last_command()
 time.sleep(1)
-keyboard.undo_last_action()
-time.sleep(1)
-keyboard.undo_last_action()
-time.sleep(1)
+
+# Выполняем команды
+keyboard.execute_commands()
+
