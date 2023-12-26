9cf0d24f5c5fe0b953b9f19b36865a0228de90b4
Alex <90724908+lofty1337@users.noreply.github.com>
2023-12-26 21:09:21 +0200

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,89 +1,72 @@
-import numpy as np
-
-
 class Array3d:
-    def __init__(self, dim0, dim1, dim2):
+    def __init__(self, dim0, dim1, dim2, data=None):
         self.__dim0 = dim0
         self.__dim1 = dim1
         self.__dim2 = dim2
-        self.__data = np.zeros(dim0 * dim1 * dim2)
+        if data is None:
+            self.__data = [[[0 for _ in range(dim2)] for _ in range(dim1)] for _ in range(dim0)]
+        else:
+            self.__data = data
 
     def __getitem__(self, indices):
         i, j, k = indices
-        idx = i * self.__dim1 * self.__dim2 + j * self.__dim2 + k
-        return self.__data[idx]
+        return self.__data[i][j][k]
 
     def __setitem__(self, indices, value):
         i, j, k = indices
-        idx = i * self.__dim1 * self.__dim2 + j * self.__dim2 + k
-        self.__data[idx] = value
+        self.__data[i][j][k] = value
+
+    def __str__(self):
+        return f"Array3d({self.__dim0}, {self.__dim1}, {self.__dim2}, {self.__data})"
 
     def get_values_0(self, i):
-        return [self.__data[i * self.__dim1 * self.__dim2 + j * self.__dim2 + k]
-                for j in range(self.__dim1)
-                for k in range(self.__dim2)]
+        return self.__data[i][0][:]
 
     def get_values_1(self, j):
-        return [self.__data[i * self.__dim1 * self.__dim2 + j * self.__dim2 + k]
-                for i in range(self.__dim0)
-                for k in range(self.__dim2)]
+        return [self.__data[i][j][k] for i in range(self.__dim0) for k in range(self.__dim2)]
 
     def get_values_2(self, k):
-        return [self.__data[i * self.__dim1 * self.__dim2 + j * self.__dim2 + k]
-                for i in range(self.__dim0)
-                for j in range(self.__dim1)]
+        return [self.__data[i][j][k] for i in range(self.__dim0) for j in range(self.__dim1)]
 
     def get_values_01(self, i, j):
-        return [self.__data[i * self.__dim1 * self.__dim2 + j * self.__dim2 + k]
-                for k in range(self.__dim2)]
+        return self.__data[i][j][:]
 
     def get_values_02(self, i, k):
-        return [self.__data[i * self.__dim1 * self.__dim2 + j * self.__dim2 + k]
-                for j in range(self.__dim2)]
+        return [self.__data[i][j][k] for j in range(self.__dim1)]
 
     def get_values_12(self, j, k):
-        return [self.__data[i * self.__dim1 * self.__dim2 + j * self.__dim2 + k]
-                for i in range(self.__dim0)]
+        return [self.__data[i][j][k] for i in range(self.__dim0)]
 
     def set_values_0(self, i, values):
-        for j in range(self.__dim1):
-            for k in range(self.__dim2):
-                self.__data[i * self.__dim1 * self.__dim2 + j * self.__dim2 + k] = values[j * self.__dim2 + k]
+        self.__data[i][0] = values
 
     def set_values_1(self, j, values):
-        for i in range(self.__dim1):
+        for i in range(self.__dim0):
             for k in range(self.__dim2):
-                self.__data[i * self.__dim1 * self.__dim2 + j * self.__dim2 + k] = values[j * self.__dim2 + k]
+                self.__data[i][j][k] = values[j * self.__dim2 + k]
 
     def set_values_2(self, k, values):
         for i in range(self.__dim0):
             for j in range(self.__dim1):
-                self.__data[i * self.__dim1 * self.__dim2 + j * self.__dim2 + k] = values[j * self.__dim1 + j]
+                self.__data[i][j][k] = values[j * self.__dim1 + j]
 
     def set_values_01(self, i, j, values):
-        for k in range(self.__dim2):
-            self.__data[i * self.__dim1 * self.__dim2 + j * self.__dim2 + k] = values[k]
+        self.__data[i][j] = values
 
     def set_values_02(self, i, k, values):
         for j in range(self.__dim1):
-            self.__data[i * self.__dim1 * self.__dim2 + j * self.__dim2 + k] = values[j]
+            self.__data[i][j][k] = values[j]
 
     def set_values_12(self, j, k, values):
         for i in range(self.__dim0):
-            self.__data[i * self.__dim1 * self.__dim2 + j * self.__dim2 + k] = values[i]
+            self.__data[i][j][k] = values[i]
+
+    @classmethod
+    def create_array_with_same_elements(cls, dim0, dim1, dim2, value):
+        return cls(dim0, dim1, dim2, [[[value for _ in range(dim2)] for _ in range(dim1)] for _ in range(dim0)])
 
 
-def create_array_with_same_elements(dim0, dim1, dim2, value):
-    return np.full((dim0, dim1, dim2), value)
-
-
-def create_array_with_zeroes(dim0, dim1, dim2):
-    return np.zeros((dim0, dim1, dim2))
-
-
-def create_array_with_ones(dim0, dim1, dim2):
-    return np.ones((dim0, dim1, dim2))
-
+# Пример использования:
 
 arr = Array3d(3, 4, 2)
 arr[0, 1, 1] = 5
@@ -111,5 +94,5 @@ arr.set_values_0(1, [1, 2, 3, 4, 5, 6, 7, 8])
 print(f"arr[1, 0, 0]: {arr[1, 0, 0]}")
 print(f"arr[1, 1, 1]: {arr[1, 1, 1]}")
 
-array_same_elements = create_array_with_same_elements(2, 3, 4, 0.5)
+array_same_elements = Array3d.create_array_with_same_elements(2, 3, 4, 0.5)
 print(f"same elements array:\n{array_same_elements}")
